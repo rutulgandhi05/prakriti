41cb54dd5e4047492ec464e78a5273a19080b1b6
RUTUL <dhaval@deftboxsolutions.com>
2024-09-12 21:03:02 +0200

    modified thechosenone/main.py

--- thechosenone/main.py
+++ thechosenone/main.py
@@ -301,7 +301,7 @@ def load_dinov2():
 
 
 if __name__ == "__main__":
-    args = config_2_args("./config/theChosenOne_fox.yaml")
+    args = config_2_args("thechosenone/config/theChosenOne_fox.yaml")
     _ = train_loop(args, args.max_loop, start_from=0)
     
     print(args)
