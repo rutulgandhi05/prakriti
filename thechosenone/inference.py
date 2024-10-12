7ec2d962d427fa8c5448a8e628ae1d1db7a77b81
RUTUL <dhaval@deftboxsolutions.com>
2024-10-12 17:39:24 +0200

    inference.py updated

--- thechosenone/inference.py
+++ thechosenone/inference.py
@@ -44,8 +44,30 @@ def generate_game_image(prompt, config_path="thechosenone/config/captain.yaml", 
     image = pipe(prompt, num_inference_steps=35, guidance_scale=7.5).images[0]
     return image
 
+
+def loop_inference(loop):
+    args = config_2_args("thechosenone/config/captain.yaml")
+
+    model_path = os.path.join(args.output_dir, args.character_name, str(loop))
+    pipe = DiffusionPipeline.from_pretrained(model_path, torch_dtype=torch.float16)
+    pipe.to("cuda")
+    pipe.load_lora_weights(os.path.join(model_path, f"checkpoint-{args.checkpointing_steps * args.num_train_epochs}"))
+
+    prompt_postfix = " sitting on a rocket."
+    image_postfix = prompt_postfix.replace(" ", "_")
+
+    # create folder
+    output_folder = f"data/inference_results/{args.character_name}"
+    os.makedirs(output_folder)
+
+    # remember to use the place holader here
+    prompt = f"A photo of {args.placeholder_token}{prompt_postfix}."
+    image = pipe(prompt, num_inference_steps=35, guidance_scale=7.5).images[0]
+    image.save(os.path.join(output_folder, f"{args.character_name}_{image_postfix}.png"))
+
+
 if __name__ == "__main__":
     for i in range(5):
+        loop_inference(i)
 
-        img = generate_game_image(prompt="generate image of c<$V$>m", loop=i)
-        img.save(f"data/cap_mavrick/loop{i}.png")
+        
