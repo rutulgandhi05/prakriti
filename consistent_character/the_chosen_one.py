63cdd1738d15fb82fa0a27b8d4fa94abcfc3ecd3
RUTUL <dhaval@deftboxsolutions.com>
2024-10-24 22:34:19 +0200

    path settings updated

--- consistent_character/the_chosen_one.py
+++ consistent_character/the_chosen_one.py
@@ -110,15 +110,26 @@ def config_2_args(path):
     return args
 
 
-def import_model_class_from_model_name_or_path(pretrained_model_name_or_path: str, revision: str, subfolder: str = "text_encoder"):
-    """Dynamically import the correct text encoder model class from the model name."""
-    text_encoder_config = PretrainedConfig.from_pretrained(pretrained_model_name_or_path, subfolder=subfolder, revision=revision)
+def import_model_class_from_model_name_or_path(
+    pretrained_model_name_or_path: str, revision: str, subfolder: str = "text_encoder"
+):
+    text_encoder_config = PretrainedConfig.from_pretrained(
+        pretrained_model_name_or_path, subfolder=subfolder, revision=revision
+    )
     model_class = text_encoder_config.architectures[0]
 
     if model_class == "CLIPTextModel":
+        from transformers import CLIPTextModel
+
         return CLIPTextModel
+    elif model_class == "CLIPTextModelWithProjection":
+        from transformers import CLIPTextModelWithProjection
+
+        return CLIPTextModelWithProjection
     else:
         raise ValueError(f"{model_class} is not supported.")
+
+
 
 
 def adjust_learning_rate(optimizer, character_consistency, threshold, high_lr, low_lr):
