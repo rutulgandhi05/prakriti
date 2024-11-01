be94dec59946a44f49008a44cd3da9bfa36a248a
RUTUL <dhaval@deftboxsolutions.com>
2024-11-01 15:25:47 +0100

    print prompt

--- thechosenone/main.py
+++ thechosenone/main.py
@@ -263,6 +263,7 @@ def generate_images(pipe: StableDiffusionXLPipeline, prompt: str, n_prompt: str,
     y_values = ["front shot", "rear angle", "side angle", "shot from above", "low angle shot"]
 
     prompt = random.choice(x_values)+prompt+""+ random.choice(y_values)+prompt+""+ random.choice(prompt_suffix)
+    print(prompt)
 
     image = pipe(prompt=prompt, num_inference_steps=infer_steps, guidance_scale=guidance_scale, negative_prompt=n_prompt).images[0]
     return image
